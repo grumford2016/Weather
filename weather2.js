a882710e735fa099801413031bf7a27c24110dfb
grumford2016@kellogg.northwestern.edu <grumford2016@kellogg.northwestern.edu>
2015-11-18 17:56:52 -0600

    very confused

--- weather2.js
+++ weather2.js
@@ -4,10 +4,22 @@ $(function() {
     console.log(JSON.stringify(data));
     
     // Put your code here. Don't change any other code in this file. You will be sad.
-    var markup = "The weather report... " + "will be here when I finish my homework.";
+    var markup= "The weather report..." + function(query)
+    $.ajax({
+      url: 'https://api.forecast.io/forecast/6dbe98374cc5b8f9ea63d5ec73de9c04/42.056459,-87.675267?callback=?',
+      data: {
+        q: query,
+        type: 'temperature'
+      },
+      success: function(response) {
+      currently.get().temperature();
+      daily.getDay(3).temperature();
+    },
+  });
+    
     // End of your code
-
-    $('.weather-report').html(markup);
+}
+  $('.weather-report').html(markup);
   }
   $('a.get-the-weather').on('click', function(event) {
     event.preventDefault();
@@ -22,63 +34,3 @@ $(function() {
 });
 
 
-
-
-
-$(function() {
-  var fetchTracks = function(albumId, callback) {
-    $.ajax({
-      url: 'https://api.spotify.com/v1/albums/' + albumId,
-      success: function(response) {
-        callback(response);
-      }
-    });
-  };
-  var searchAlbums = function(query) {
-    $.ajax({
-      url: 'https://api.spotify.com/v1/search',
-      data: {
-        q: query,
-        type: 'album'
-      },
-      success: function(response) {
-        // Set a window-level variable so we can use it in the console
-        window.response = response;
-
-        // Write the response from Spotify to the console, in pretty collapsible way
-        console.log(response);
-
-        // See all the guts
-        console.log(JSON.stringify(response));
-
-        // Empty the existing album list
-        $('.album-list').show().empty()
-
-        // Fill the album list with each album in the response
-        $.each(response.albums.items, function(index, album) {
-          var markup = '<img src="' + album.images[0].url +'" data-album-id="' + album.id + '">';
-          $('.album-list').append(markup);
-        });
-      }
-    });
-  };
-  $('form').on('submit', function(event) {
-    event.preventDefault();
-    searchAlbums($('#artist').val());
-  });
-  $(document).on('click', '.album-list img', function(event) {
-    fetchTracks($(this).data('album-id'), function(data) {
-      if (window.audioObject !== undefined) {
-        window.audioObject.pause();
-      }
-      window.audioObject = new Audio(data.tracks.items[0].preview_url);
-      window.audioObject.play();
-    });
-  });
-  $(document).on('click', '.stop-it', function(event) {
-    event.preventDefault();
-    if (window.audioObject !== undefined) {
-      window.audioObject.pause();
-    }
-  });
-});
