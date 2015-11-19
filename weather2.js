f3ae002948d57396238a08adccb73b229abaff9f
grumford2016@kellogg.northwestern.edu <grumford2016@kellogg.northwestern.edu>
2015-11-19 14:17:38 -0600

    New changes

--- weather2.js
+++ weather2.js
@@ -4,22 +4,15 @@ $(function() {
     console.log(JSON.stringify(data));
     
     // Put your code here. Don't change any other code in this file. You will be sad.
-    var markup= "The weather report..." + function(query)
-    $.ajax({
-      url: 'https://api.forecast.io/forecast/6dbe98374cc5b8f9ea63d5ec73de9c04/42.056459,-87.675267?callback=?',
-      data: {
-        q: query,
-        type: 'temperature'
-      },
-      success: function(response) {
-      currently.get().temperature();
-      daily.getDay(3).temperature();
-    },
-  });
-    
-    // End of your code
-}
-  $('.weather-report').html(markup);
+    var markup = "Today's temperature is " + data.currently.apparentTemperature + " degrees F!  Wow! "+
+    "   Tomorrow's high will be " + data.daily.data[1].temperatureMax + " degrees F and tomorrow's low will be " + data.daily.data[1].temperatureMin + " degrees F. " + data.daily.data[1].summary +
+    "   The day after tomorrow's high will be " + data.daily.data[2].temperatureMax + " degrees F and the low will be " + data.daily.data[2].temperatureMin + " degrees F. " + data.daily.data[2].summary +
+    "   Day 3's high will be " + data.daily.data[3].temperatureMax + " degrees F and the low will be " + data.daily.data[3].temperatureMin + " degrees F. " + data.daily.data[3].summary +  
+    "   Day 4's high will be " + data.daily.data[3].temperatureMax + " degrees F and the low will be " + data.daily.data[3].temperatureMin + " degrees F. " + data.daily.data[3].summary;
+
+
+
+    $('.weather-report').html(markup);
   }
   $('a.get-the-weather').on('click', function(event) {
     event.preventDefault();
